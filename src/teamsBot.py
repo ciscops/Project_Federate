8a72903087b05ba769f9494181f098a66629c4af
Danielle <dastacy@cisco.com>
2021-04-14 17:21:37 -0400

    cleaning up

--- src/teamsBot.py
+++ src/teamsBot.py
@@ -5,7 +5,7 @@ def send_Teams_Message_Dnac(mksft_teams, events):
     myTeamsMessage = pymsteams.connectorcard(mksft_teams)
 
     for event in events:
-        myTeamsMessage.text('DNAC Event         ' + event['name'] + ': ' + event['description'])
+        myTeamsMessage.text('DNAC Event: ' + event['name'] + ': ' + event['description'])
         myTeamsMessage.send()
 
 
@@ -16,5 +16,5 @@ def send_Teams_Message_Prime(mksft_teams, alarms):
         alarm_name = alarm['queryResponse']['entity'][0]['alarmsDTO']['condition']['value']
         alarm_description = alarm['queryResponse']['entity'][0]['alarmsDTO']['message']
 
-        myTeamsMessage.text('Prime Alarm        ' + alarm_name + ': ' + alarm_description)
+        myTeamsMessage.text('Prime Alarm: ' + alarm_name + ': ' + alarm_description)
         myTeamsMessage.send()
