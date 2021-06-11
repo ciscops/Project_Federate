b2e7b59306d91ccfea013b9b4c24e8b453d74474
Danielle <dastacy@cisco.com>
2021-06-11 13:51:08 -0400

    adding checkIp function

---
+++ src/checkIp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import nmap, socket
+
+def checkIp(ip_addr):
+    scanner = nmap.PortScanner()
+
+    try:
+        host = socket.gethostbyname(ip_addr)
+        scanner.scan(host, '1', '-v')
+
+        if scanner[host].state() == 'up':
+            return True
+
+        return False
+
+    except:
+        e = sys.exc_info()[0]
+        print("Error: ", str(e))
+
+        return False
